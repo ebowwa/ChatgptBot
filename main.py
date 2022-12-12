e7e51889ce2ecb52d8f3f8edfd4d21daf75d2bbf
ebowwa <81942069+ebowwa@users.noreply.github.com>
2022-12-11 23:00:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
-# Import the ChatGPT and vscode libraries
-import chatgpt
 import vscode
+import chatgpt
+import code_model
+import ui
+import commands
+import messages
 
-# Define a function for generating responses
-def generate_response(user_input):
-  # Use the ChatGPT library to get a response
-  response = chatgpt.get_response(user_input)
+def main():
+    # Create a ChatGPT instance and load the trained model
+    chatbot = chatgpt.ChatGPT()
+    chatbot.load_model("model.h5")
+    
+    # Create a CodeModel instance and load the trained model
+    codemodel = code_model.CodeModel()
+    codemodel.load_model("codemodel.h5")
+    
+    # Show the user interface and command palette
+    ui.show()
+    
+    # Listen for user input and generate responses or code suggestions
+    while True:
+        user_input = ui.get_input()
+        if user_input == "":
+            continue
+        elif user_input == "exit":
+            break
+        elif user_input.startswith("code "):
+            # Generate code suggestions
+            code_input = user_input[5:]
+            code_output = codemodel.generate_code(code_input)
+            ui.show_code(code_output)
+        elif user_input.startswith("insert "):
+            # Insert generated code into the editor
+            code_input = user_input[7:]
+            code_output = codemodel.generate_code(code_input)
+            vscode.window.activeTextEditor.insertCode(code_output)
+        else:
+            # Generate chatbot responses
+            chatbot_output = chatbot.generate_response(user_input)
+            ui.show_response(chatbot_output)
+    
+    # Clean up and exit
+    ui.hide()
 
-  # Return the response
-  return response
-
-# Define a function for creating a new file
-def create_file(file_name, code):
-  # Use the vscode API to create a new file
-  file = vscode.workspace.create_file(file_name)
-
-  # Use the vscode API to write the code to the file
-  file.write(code)
-
-# Define a function for creating a new folder
-def create_folder(folder_name):
-  # Use the vscode API to create a new folder
-  folder = vscode.workspace.create_folder(folder_name)
-
-# Define a function for refactoring code
-def refactor_code(code):
-  # Use the ChatGPT library to refactor the code
-  refactored_code
+if __name__ == "__main__":
+    main()
